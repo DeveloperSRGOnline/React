330cf20e845deb39765ea6d8aee466a8219f8718
Shivam <shivamgarade05@gmail.com>
2026-08-25 18:50:32 +0530

    plane react project

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <>
+      <h1>Hellow world | shivam Garade</h1>
+      <p>In this lecture i learned about how core react folder looks like and how any other bundler like vite's react folder looks like. Then we eleminate what not need and what are the bari minimun thing and added our curstom text inside application. Did some manipulation</p>
+    </>
+  )
+}
+
+export default App
