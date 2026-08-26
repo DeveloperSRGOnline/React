b42de9e84a93f5560460842fcc3293c41d163c8a
Shivam <shivamgarade05@gmail.com>
2026-08-26 23:15:00 +0530

    feat: add custom MyApp component and example React elements

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,6 +1,32 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import React from 'react'
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom app function</h1>
+    </div>
+  )
+}
+
+const anotherElement = (
+    <a href="https://google.com">Visit Google</a>
+)
+
+const anotherUser =   "chai aur react"
+
+const reactElement = React.createElement(
+  'a',
+  {href:'https://google.com',target:'_blank'},
+  'I am custom react element',
+  anotherUser // and agar yaha koi let say if syntax likhoge to vo kaise chalega
+)
 
 createRoot(document.getElementById('root')).render(
+  // anotherElement
+  // reactElement
+  // <MyApp />
+  // MyApp()
   <App />
 )
