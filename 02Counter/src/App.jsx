6a7bfdb11ea0a2706eb384c381e7a6167d9b13ec
Shivam <shivamgarade05@gmail.com>
2026-08-29 00:36:09 +0530

    refactor: update counter logic to use functional state updates

--- 02Counter/src/App.jsx
+++ 02Counter/src/App.jsx
@@ -11,11 +11,28 @@ function App() {
     // console.log("hello from my side")
     // counter = counter + 1;
     // console.log(counter)
-    if (counter < 20) setCounter(counter + 1)
+    // if (counter < 20) setCounter(counter + 1)
+
+    // Interview que
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)// batch mein same kaam hoga
+
+    // right way if you want to do that
+    setCounter((prevCount) => prevCount + 1)
+    // setCounter((prevCount) => prevCount + 1)
+    // setCounter((prevCount) => prevCount + 1)
+    // setCounter((prevCount) => prevCount + 1)
   }
 
   const removeValue = () => {
-    if(counter > 0) setCounter(counter - 1)
+    // if(counter > 0) setCounter(counter - 1)
+    setCounter(counter - 1)
+    setCounter(counter - 1)
+    setCounter(counter - 1)
+    setCounter(counter - 1)
   }
 
   // React react karti hai variables ke updation pe isliye use react kehete hai , badi hi reactive hai
